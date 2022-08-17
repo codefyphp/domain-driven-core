569cbfa31bc9573ec3726e2688ce7a6968143f54
Joshua Parker <josh@joshuaparker.blog>
2022-08-17 12:49:10 -0700

    Added identity map trait.

---
+++ Traits/IdentityMapAware.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * CodefyPHP
+ *
+ * @link       https://github.com/codefyphp/codefy
+ * @copyright  2022 Joshua Parker <josh@joshuaparker.blog>
+ * @license    https://opensource.org/licenses/mit-license.php MIT License
+ *
+ * @since      1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Codefy\Traits;
+
+use Codefy\Domain\Aggregate\AggregateId;
+use Codefy\Domain\Aggregate\RecordsEvents;
+
+use function Qubus\Support\Helpers\is_true__;
+
+trait IdentityMapAware
+{
+    protected array $identityMap = [];
+    protected bool $enableIdentityMap = true;
+
+    /**
+     * Attach an aggregate to the map.
+     *
+     * @param RecordsEvents $aggregate
+     * @return void
+     */
+    public function attachToIdentityMap(RecordsEvents $aggregate): void
+    {
+        if (is_true__($this->enableIdentityMap)) {
+            $this->identityMap[$aggregate->aggregateId()->__toString()] = $aggregate;
+        }
+    }
+
+    /**
+     * Retrieve an aggregate from the map by its aggregate id.
+     * @param AggregateId $aggregateId
+     * @return RecordsEvents|null
+     */
+    public function retrieveFromIdentityMap(AggregateId $aggregateId): RecordsEvents|null
+    {
+        if (is_true__($this->enableIdentityMap) && isset($this->identityMap[$aggregateId->__toString()])) {
+            return $this->identityMap[$aggregateId->__toString()];
+        }
+
+        return null;
+    }
+
+    /**
+     * Clear the identity map.
+     *
+     * @return void
+     */
+    public function clearIdentityMap(): void
+    {
+        $this->identityMap = [];
+    }
+
+    /**
+     * Remove aggregate from identity map.
+     *
+     * @param RecordsEvents $aggregate
+     * @return void
+     */
+    public function removeFromIdentityMap(RecordsEvents $aggregate): void
+    {
+        if (
+            is_true__($this->enableIdentityMap) && isset(
+                $this->identityMap[$aggregate->aggregateId()->__toString()]
+            )
+        ) {
+            unset($this->identityMap[$aggregate->aggregateId()->__toString()]);
+        }
+    }
+
+    /**
+     * Set whether identity map is enabled.
+     *
+     * @param bool $bool
+     * @return $this
+     */
+    public function enableIdentityMap(bool $bool = true): static
+    {
+        $this->enableIdentityMap = $bool;
+
+        return $this;
+    }
+}
